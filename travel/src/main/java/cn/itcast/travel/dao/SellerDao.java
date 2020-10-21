10278775329bd27c4eb681d10de48e7a76ed6d9a
Kingyumu <57080807+Kingyumu@users.noreply.github.com>
2020-10-22 00:28:36 +0800

    Add files via upload

---
+++ travel/src/main/java/cn/itcast/travel/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package cn.itcast.travel.dao;
+
+import cn.itcast.travel.domain.Seller;
+
+public interface SellerDao {
+
+    /**
+     * 根据id查询
+     * @param sid
+     * @return
+     */
+    public Seller findById(int sid);
+
+}
